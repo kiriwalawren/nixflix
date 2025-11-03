6bb218eb40d1395b0c1422919f080d7eff405f68
Kiri Carlson <kiri@walawren.com>
2025-11-03 04:15:59 -0500

    auto discover vm tests

--- tests/vm-tests/default.nix
+++ tests/vm-tests/default.nix
@@ -2,14 +2,22 @@
   system ? builtins.currentSystem,
   pkgs ? import <nixpkgs> {inherit system;},
   nixosModules,
-}: {
-  sonarr-basic = import ./sonarr-basic.nix {inherit system pkgs nixosModules;};
-  radarr-basic = import ./radarr-basic.nix {inherit system pkgs nixosModules;};
-  lidarr-basic = import ./lidarr-basic.nix {inherit system pkgs nixosModules;};
-  prowlarr-basic = import ./prowlarr-basic.nix {inherit system pkgs nixosModules;};
-  sabnzbd-basic = import ./sabnzbd-basic.nix {inherit system pkgs nixosModules;};
-  full-stack = import ./full-stack.nix {inherit system pkgs nixosModules;};
-  nginx-integration = import ./nginx-integration.nix {inherit system pkgs nixosModules;};
-  postgresql-integration = import ./postgresql-integration.nix {inherit system pkgs nixosModules;};
-  mullvad-integration = import ./mullvad-integration.nix {inherit system pkgs nixosModules;};
-}
+}: let
+  # Get all .nix files in the current directory except default.nix
+  testFiles = builtins.filter
+    (name: name != "default.nix" && pkgs.lib.hasSuffix ".nix" name)
+    (builtins.attrNames (builtins.readDir ./.));
+
+  # Convert filename to test name (e.g., "sonarr-basic.nix" -> "sonarr-basic")
+  testNameFromFile = file: pkgs.lib.removeSuffix ".nix" file;
+
+  # Import each test file with the required arguments
+  importTest = file: import (./. + "/${file}") {inherit system pkgs nixosModules;};
+in
+  # Create an attribute set of all tests
+  builtins.listToAttrs (
+    map (file: {
+      name = testNameFromFile file;
+      value = importTest file;
+    }) testFiles
+  )
