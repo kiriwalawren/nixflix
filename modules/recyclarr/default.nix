22f650a95e454bcde6dad4a8c6e6b2769e7bc950
Kiri Carlson <kiri@walawren.com>
2025-11-08 00:50:34 -0500

    configure full defaults for sonarr and radarr

--- modules/recyclarr/default.nix
+++ modules/recyclarr/default.nix
@@ -8,92 +8,14 @@ with lib; let
   inherit (config.nixflix) globals;
   cfg = config.nixflix.recyclarr;
 
-  sonarrConfig = optionalAttrs cfg.sonarr.enable {
-    sonarr = {
-      sonarr_main = {
-        base_url = "http://127.0.0.1:${toString config.nixflix.sonarr.config.hostConfig.port}${toString config.nixflix.sonarr.config.hostConfig.urlBase}";
-        api_key._secret = "/run/credentials/recyclarr.service/sonarr-api_key";
-        media_naming = {
-          series = "default";
-          season = "default";
-          episodes = {
-            rename = true;
-            standard = "default";
-            daily = "default";
-            anime = "default";
-          };
-        };
+  configOption = import ./config-option.nix {inherit lib;};
 
-        include =
-          [
-            {template = "sonarr-quality-definition-series";}
-            {template = "sonarr-v4-quality-profile-web-2160p-alternative";}
-            {template = "sonarr-v4-custom-formats-web-2160p";}
-          ]
-          ++ optionals cfg.sonarr.anime.enable [
-            {template = "sonarr-quality-definition-anime";}
-            {template = "sonarr-v4-quality-profile-anime";}
-            {template = "sonarr-v4-custom-formats-anime";}
-          ];
-      };
-    };
-  };
-
-  radarrConfig = optionalAttrs cfg.radarr.enable {
-    radarr = {
-      radarr_main = {
-        base_url = "http://127.0.0.1:${toString config.nixflix.radarr.config.hostConfig.port}${toString config.nixflix.radarr.config.hostConfig.urlBase}";
-        api_key._secret = "/run/credentials/recyclarr.service/radarr-api_key";
-        media_naming = {
-          folder = "default";
-          movie = {
-            rename = true;
-            standard = "standard";
-          };
-        };
-
-        quality_profiles = [
-          {
-            name = "UHD Bluray + WEB";
-            reset_unmatched_scores.enabled = true;
-            upgrade = {
-              allowed = true;
-              until_quality = "Bluray-2160p";
-              until_score = 10000;
-            };
-            min_format_score = 0;
-            quality_sort = "top";
-            qualities = [
-              {name = "Bluray-2160p";}
-              {
-                name = "WEB-2160p";
-                qualities = ["WEBDL-2160p" "WEBRip-2160p"];
-              }
-              {name = "Bluray-1080p";}
-              {
-                name = "WEB-1080p";
-                qualities = ["WEBDL-1080p" "WEBRip-1080p"];
-              }
-              {name = "Bluray-720p";}
-            ];
-          }
-        ];
-
-        include =
-          [
-            {template = "radarr-quality-definition-movie";}
-            {template = "radarr-custom-formats-uhd-bluray-web";}
-          ]
-          ++ optionals cfg.radarr.anime.enable [
-            {template = "radarr-quality-definition-anime";}
-            {template = "radarr-quality-profile-anime";}
-            {template = "radarr-custom-formats-anime";}
-          ];
-      };
-    };
-  };
-
-  recyclarrConfiguration = sonarrConfig // radarrConfig;
+  sonarrConfig = optionalAttrs cfg.sonarr.enable (import ./sonarr-default.nix {inherit config lib;});
+  radarrConfig = optionalAttrs cfg.radarr.enable (import ./radarr-default.nix {inherit config lib;});
+  effectiveConfiguration =
+    if cfg.config == null
+    then sonarrConfig // radarrConfig
+    else cfg.config;
 in {
   options.nixflix.recyclarr = {
     enable = mkOption {
@@ -112,16 +34,6 @@ in {
       type = types.str;
       default = "recyclarr";
       description = "Group under which Recyclarr runs";
-    };
-
-    config = mkOption {
-      type = types.nullOr types.attrs;
-      default = null;
-      description = ''
-        Recyclarr configuration as a Nix attribute set.
-        When set, this completely replaces the auto-generated configuration,
-        giving you full control over the Recyclarr setup.
-      '';
     };
 
     sonarr = {
@@ -165,6 +77,8 @@ in {
         };
       };
     };
+
+    config = configOption;
   };
 
   config = mkIf (nixflix.enable && cfg.enable) {
@@ -194,10 +108,7 @@ in {
     services.recyclarr = {
       enable = true;
       inherit (cfg) user group;
-      configuration =
-        if cfg.config == null
-        then recyclarrConfiguration
-        else cfg.config;
+      configuration = effectiveConfiguration;
       schedule = "daily";
     };
 
