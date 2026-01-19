a7618cf6edcd574d0d532e218a00b16d3cd21f3e
Kiri Carlson <kiri@walawren.com>
2026-01-18 20:30:47 -0500

    Updated tailscale documentation to fix tailscale node direct connections

--- modules/mullvad.nix
+++ modules/mullvad.nix
@@ -32,12 +32,17 @@ in {
                 chain prerouting {
                   type filter hook prerouting priority -100; policy accept;
                   ip saddr 100.64.0.0/10 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+                  udp dport 41641 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+                  # Allow incoming UDP on Tailscale port to bypass Mullvad
+                  udp dport 41641 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
                 }
 
                 chain outgoing {
                   type route hook output priority -100; policy accept;
                   meta mark 0x80000 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
                   ip daddr 100.64.0.0/10 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+                  # Allow outgoing UDP from Tailscale port to bypass Mullvad
+                  udp sport 41641 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
                 }
               ''';
             };
