936cd46dc078f91a9be89a992d9bc6d87e7a2462
Kiri Carlson <kiri@walawren.com>
2026-05-04 14:43:44 -0400

    fix caddy syntax error

--- lib/mkVirtualHosts.nix
+++ lib/mkVirtualHosts.nix
@@ -75,16 +75,29 @@ let
     let
       themeParkTag =
         if themeParkService == null then "</body>" else themeParkTags.${themeParkService} or "</body>";
+
+      subdirectives =
+        lib.optional (themeParkService != null && cfg.theme.enable) "header_up -Accept-Encoding"
+        ++ lib.optional disableBuffering "flush_interval -1";
+
+      reverseProxyMatcher = "http://${upstreamHost}:${toString port}";
+
+      reverseProxyBlock =
+        if subdirectives == [ ] then
+          "reverse_proxy ${reverseProxyMatcher}"
+        else
+          ''
+            reverse_proxy ${reverseProxyMatcher} {
+              ${lib.concatStringsSep "\n  " subdirectives}
+            }
+          '';
+
     in
     lib.mkIf cfg.caddy.enable {
       extraConfig = ''
         ${tlsDirective}
 
-        reverse_proxy http://${upstreamHost}:${toString port} ${lib.optionalString disableBuffering ''
-          {
-            flush_interval -1
-          }
-        ''}
+        ${reverseProxyBlock}
 
         ${lib.concatMapStringsSep "\n" (h: "header_down -${h}") stripHeaders}
 
@@ -92,7 +105,6 @@ let
           replace {
             ${themeParkTag} "<link rel=\"stylesheet\" type=\"text/css\" href=\"${themeParkUrl themeParkService}\">${themeParkTag}"
           }
-          header_up -Accept-Encoding
         ''}
 
         ${extraConfig}
