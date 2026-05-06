95d23c8dc4a80f519a30a419f622e60452693778
Agahnim <43116178+naguiagahnim@users.noreply.github.com>
2026-05-06 09:16:36 +0200

    format default.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,7 @@
-{pkgs ? import <nixpkgs> {}}: let
+{
+  pkgs ? import <nixpkgs> { },
+}:
+let
   lock = builtins.fromJSON (builtins.readFile ./flake.lock);
   vpn-confinement-src = fetchTarball {
     url = "https://github.com/Maroka-chan/VPN-Confinement/archive/${lock.nodes.vpn-confinement.locked.rev}.tar.gz";
@@ -10,9 +13,10 @@
       "${vpn-confinement-src}/modules/vpn-netns.nix"
     ];
   };
-in {
+in
+{
   nixosModules.default = nixflixModule;
   nixosModules.nixflix = nixflixModule;
-  lib.jellyfinPlugins = import ./lib/jellyfin-plugins.nix {inherit (pkgs) lib;};
-  lib.buildJellyfinPlugin = import ./lib/build-jellyfin-plugin.nix {inherit pkgs;};
+  lib.jellyfinPlugins = import ./lib/jellyfin-plugins.nix { inherit (pkgs) lib; };
+  lib.buildJellyfinPlugin = import ./lib/build-jellyfin-plugin.nix { inherit pkgs; };
 }
